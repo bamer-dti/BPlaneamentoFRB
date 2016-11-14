5e191b096239cdd41f827f662c20465c7e64dfcc
bamer-dti <miguel.silva@bamer.pt>
2016-11-14 17:03:38 +0000

    beta version

--- src/main/java/utils/ValoresDefeito.java
+++ src/main/java/utils/ValoresDefeito.java
@@ -5,13 +5,6 @@ public class ValoresDefeito {
     //GLOBALS
     public static final String TITULO_APP = "BPlaneamento FRB BETA";
 
-    //SQL
-    public static final String SQL_SERVIDOR = "192.168.0.3";
-    public static final int SQL_PORTA = 1433;
-    public static final String SQL_DB = "bamer";
-    public static final String SQL_USER = "printserver";
-    public static final String SQL_PASSWORD = "12345";
-
     //SQLITE
     public static final String BD_PREFERENCIAS = "dbprefs";
 
