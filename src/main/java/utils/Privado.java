ec36484a38a83e1dc89008f4c737aca53a27d543
bamer-dti <miguel.silva@bamer.pt>
2017-08-17 12:30:59 +0100

    3.1.2 Correcções e updates

---
+++ src/main/java/utils/Privado.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Privado {
+    public static final boolean TESTING = false; //Se sim, só faz o load de BO e Notas
+
+    public static final String URL_ALTERAR_ORDEM = "http://server.bamer.pt:99/bameros.svc/actualizar_ordem_planeamento";
+    public static final String URL_ACTUALIZAR_COMPRMISSO = "http://server.bamer.pt:99/bameros.svc/actualizar_compromisso_planeamento";
+    public static final String URL_ACTUALIZAR_COR = "http://server.bamer.pt:99/bameros.svc/actualizar_cor";
+    public static final String URL_ENVIAR_SMS = "http://server.bamer.pt:99/bameros.svc/firebasesms";
+    public static final String URL_ACTUALIZAR_OSBO = "http://server.bamer.pt:99/bameros.svc/updateosbo";
+}
